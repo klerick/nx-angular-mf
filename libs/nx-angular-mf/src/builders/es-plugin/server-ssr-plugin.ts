d6ab6a0471f29b3f3dc012a9bcd975a0339e9b56
Alex H <klerick666@gmail.com>
2024-12-27 18:23:42 +0100

    Step 3 - Register loader via esbuild: Create server.ssr.mjs to initialize loader before SSR server start.

---
+++ libs/nx-angular-mf/src/builders/es-plugin/server-ssr-plugin.ts
@@ -0,0 +1,50 @@
+import { Plugin, PluginBuild } from 'esbuild';
+import { DEPLOY_URL } from '../custom-loader/custom-loader';
+
+
+export function serverSSRPlugin(deployUrl: string): Plugin {
+  return {
+    name: 'serverSSRPlugin',
+    setup(build: PluginBuild) {
+      if (build.initialOptions.platform !== 'node') return;
+      const serverSrrName = 'server.ssr';
+
+      build.initialOptions.entryPoints = {
+        ...build.initialOptions.entryPoints,
+        [serverSrrName]: serverSrrName,
+      };
+      build.initialOptions.external = [
+        ...build.initialOptions.external,
+        './server.mjs',
+      ];
+      build.onResolve(
+        { filter: new RegExp('^' + serverSrrName) },
+        ({ kind, path }) => {
+          if (kind !== 'entry-point') {
+            return null;
+          }
+          return {
+            path: path,
+            namespace: serverSrrName,
+          };
+        }
+      );
+      build.onLoad({ filter: /./, namespace: serverSrrName }, () => {
+        return {
+          contents: `
+            import {register} from "node:module";
+            const { port1, port2 } = new MessageChannel();
+            register('./custom-loader.mjs', {
+              parentURL: import.meta.url,
+              data: { port: port2 },
+              transferList: [port2],
+            });
+            port1.postMessage({ kind: '${DEPLOY_URL}', result: '${deployUrl}' });
+            import('./server.mjs')
+          `,
+          loader: 'js',
+        };
+      });
+    },
+  };
+}
